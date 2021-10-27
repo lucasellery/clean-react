9bc4e102e5dba737f16e06755f6b48758e1c01df
Lucas Ellery <lucas.ellery@somapay.com.br>
2021-10-27 13:33:24 -0300

    test: ensure RemoteAuth calls HttpPostClient with correct body

--- src/data/test/mockHttpClient.ts
+++ src/data/test/mockHttpClient.ts
@@ -1,10 +1,16 @@
-import { HttpPostClient, HttpPostParams } from '../protocols/http/httpPostClient';
+import {
+  HttpPostClient,
+  HttpPostParams,
+} from "../protocols/http/httpPostClient";
 
-export class HttpPostClientSpy implements HttpPostClient { //captura valores para comparar
-  url?: string
+export class HttpPostClientSpy implements HttpPostClient {
+  //captura valores para comparar
+  url?: string;
+  body?: object;
 
-  async post (params: HttpPostParams): Promise<void> {
-    this.url = params.url
-    return Promise.resolve()
+  async post(params: HttpPostParams): Promise<void> {
+    this.url = params.url;
+    this.body = params.body;
+    return Promise.resolve();
   }
 }
